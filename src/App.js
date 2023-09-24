7ba7a5050006b4d7a6885792ce79a424d56bf220
Hoda96 <farahani.h40@gmail.com>
2023-09-24 11:07:43 +0330

    react-quiz-v.1.0.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <p>React Questions</p>
+      </main>
+    </div>
+  );
+}
